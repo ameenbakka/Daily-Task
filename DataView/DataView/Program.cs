1beb824cd21e322cd37e276bcf6bde1aa6af5537
ameen <ameensahil1323@gmail.com>
2025-03-26 10:05:00 +0530

    dataview

---
+++ DataView/DataView/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;    
+namespace DataViews
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=ASlam;Database=books;IntegratedSecurity=True";
+            
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("Select *from books", conn);
+                DataSet ds = new DataSet(); 
+                adapter.Fill(ds,"books");
+                DataView view=new DataView(ds.Tables["books"]);
+                view.Sort("title ASC");
+
+            }
+        }
+  
+}
